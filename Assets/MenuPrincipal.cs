e2bbd65839ec9dab5a5c021960b7decb41c35e25
Patricio Maximiliano Zambrano Rubilar <patricio.zambrano2001@alumnos.ubiobio.cl>
2024-07-09 18:40:52 -0400

    menu

--- Assets/MenuPrincipal.cs
+++ Assets/MenuPrincipal.cs
@@ -1,12 +1,20 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.XR.Management;
 
 public class MenuPrincipal : MonoBehaviour
 {
+    void Start()
+    {
+        XRGeneralSettings.Instance.Manager.StopSubsystems();
+        XRGeneralSettings.Instance.Manager.DeinitializeLoader();
+    }
+
     public void jugar()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+      
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        
     }
 }
